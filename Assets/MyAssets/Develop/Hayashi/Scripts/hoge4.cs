196f204eff913145652fa7821d04222cc366245f
woods-one <0820ricer@gmail.com>
2023-09-19 19:06:55 +0900

    実装 : 9*9のパズルの実装

--- Assets/MyAssets/Develop/Hayashi/Scripts/hoge4.cs
+++ Assets/MyAssets/Develop/Hayashi/Scripts/hoge4.cs
@@ -10,23 +10,32 @@ public class hoge4 : MonoBehaviour
     private PuzzleState _puzzleState;
 
     private char[,] array = new char[3, 3];
+
+    private string hoge = "";
     
     public void Clicked()
     {
-        for (int i = 0; i < 3; i++)
+        for (int i = 0; i < 9; i++)
         {
-            for (int j = 0; j < 3; j++)
+            hoge += "{";
+            for (int j = 0; j < 9; j++)
             {
                 if (_puzzleState.SquareArray[i, j])
                 {
-                    array[i,j] = '○';
+                    hoge += "◎";
                 }
                 else
                 {
-                    array[i,j] = '×';
+                    hoge += "×";
+                }
+
+                if (j != 8)
+                {
+                    hoge += ',';
                 }
             }
+            hoge += "}\n";
         }
-        Debug.Log($"{array[0,0]},{array[0,1]},{array[0,2]}\n{array[1,0]},{array[1,1]},{array[1,2]}\n{array[2,0]},{array[2,1]},{array[2,2]}");
+        Debug.Log(hoge);
     }
 }
