0a4ceaa9f09ee6fe9a509a3dcc36e011ff810c8d
woods-one <0820ricer@gmail.com>
2023-09-19 15:16:37 +0900

    追加 : それぞれのマスの情報を追加

---
+++ Assets/MyAssets/Normal/Scripts/PuzzeleButton.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Tyranno.Puzzle
+{
+    public class PuzzleButton : MonoBehaviour
+    {
+        [SerializeField]
+        private PuzzleState _puzzleState;
+    }
+}
