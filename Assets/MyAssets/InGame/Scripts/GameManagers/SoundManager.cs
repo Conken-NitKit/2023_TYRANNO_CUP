3598ded812886737bb4e0eb134448f9884c1dfdd
woods-one <0820ricer@gmail.com>
2023-09-21 12:45:42 +0900

    実装 : rannkinnguwo

--- Assets/MyAssets/InGame/Scripts/GameManagers/SoundManager.cs
+++ Assets/MyAssets/InGame/Scripts/GameManagers/SoundManager.cs
@@ -6,96 +6,88 @@ using Tyranno.GameManagers;
 using UniRx;
 using UnityEngine;
 
-/// <summary>
-/// 
-/// </summary>
-public class SoundManager : MonoBehaviour
+namespace Tyranno.GameManager
 {
-    [SerializeField]
-    private AudioSource _seSource;
-    
-    [SerializeField]
-    private AudioSource _bgmSource;
-    
-    [SerializeField]
-    private AudioClip _countDownSE;
-
-    [SerializeField] 
-    private AudioClip _mainBGM;
-    
-    [SerializeField]
-    private AudioClip _finishSE;
-    
-    [SerializeField]
-    private AudioClip _stampSE;
-
-    [SerializeField] 
-    private AudioClip _correctSE;
-    
-    [SerializeField] 
-    private AudioClip _incorrectSE;
-    
-    [SerializeField] 
-    private AudioClip _resultBGM;
-    
-    [SerializeField]
-    private MainGameManager _mainGameManager;
-
-    [SerializeField] 
-    private PuzzleManager _puzzleManager;
-
-    void Start()
+    /// <summary>
+    /// 
+    /// </summary>
+    public class SoundManager : MonoBehaviour
     {
-        _mainGameManager.CurrentGameState.Subscribe(x =>
+        [SerializeField] private AudioSource _seSource;
+
+        [SerializeField] private AudioSource _bgmSource;
+
+        [SerializeField] private AudioClip _countDownSE;
+
+        [SerializeField] private AudioClip _mainBGM;
+
+        [SerializeField] private AudioClip _finishSE;
+
+        [SerializeField] private AudioClip _stampSE;
+
+        [SerializeField] private AudioClip _correctSE;
+
+        [SerializeField] private AudioClip _incorrectSE;
+
+        [SerializeField] private AudioClip _resultBGM;
+
+        [SerializeField] private MainGameManager _mainGameManager;
+
+        [SerializeField] private PuzzleManager _puzzleManager;
+
+        void Start()
         {
-            if (x == GameState.Ready)
+            _mainGameManager.CurrentGameState.Subscribe(x =>
             {
-                _seSource.clip = _countDownSE;
-                _seSource.Play();
+                if (x == GameState.Ready)
+                {
+                    _seSource.clip = _countDownSE;
+                    _seSource.Play();
 
-                _bgmSource.clip = _mainBGM;
-                _bgmSource.Play();
-            }
-            else if (x == GameState.Result)
+                    _bgmSource.clip = _mainBGM;
+                    _bgmSource.Play();
+                }
+                else if (x == GameState.Result)
+                {
+                    _bgmSource.Stop();
+                    _seSource.clip = _finishSE;
+                    _seSource.Play();
+
+                    _bgmSource.volume = 0f;
+
+                    _bgmSource.clip = _resultBGM;
+                    _bgmSource.Play();
+
+                    DOTween.To
+                    (
+                        () => _bgmSource.volume,
+                        (x) => _bgmSource.volume = x, //何を
+                        0.1f, //どこまで(最終的な値)
+                        10f //どれくらいの時間
+                    );
+                }
+            });
+
+            _puzzleManager.IsConditionMet.Skip(1).Subscribe(x =>
             {
-                _bgmSource.Stop();
-                _seSource.clip = _finishSE;
-                _seSource.Play();
+                if (x)
+                {
+                    _seSource.clip = _correctSE;
+                    _seSource.Play();
+                }
+                else
+                {
+                    _seSource.clip = _incorrectSE;
+                    _seSource.Play();
+                }
+            });
+        }
 
-                _bgmSource.volume = 0f;
-                
-                _bgmSource.clip = _resultBGM;
-                _bgmSource.Play();
-                
-                DOTween.To 
-                (
-                    () => _bgmSource.volume, 
-                    (x) => _bgmSource.volume = x,	//何を
-                    0.1f,		//どこまで(最終的な値)
-                    10f		//どれくらいの時間
-                );
-            }
-        });
-
-        _puzzleManager.IsConditionMet.Skip(1).Subscribe(x =>
+        public void RingStampSE()
         {
-            if (x)
-            {
-                _seSource.clip = _correctSE;
-                _seSource.Play();
-            }
-            else
-            {
-                _seSource.clip = _incorrectSE;
-                _seSource.Play();
-            }
-        });
-    }
-
-    public void RingStampSE()
-    {
-        _bgmSource.Stop();
-        _seSource.clip = _stampSE;
-        _seSource.Play();
+            _bgmSource.Stop();
+            _seSource.clip = _stampSE;
+            _seSource.Play();
+        }
     }
 }
